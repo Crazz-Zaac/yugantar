f7fef495ab89a8acd2f3159ef2f93d7e1dd7ca33
crazz-zaac <r.alpha919@gmail.com>
2025-10-16 21:51:44 +0200

    minor update

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import user
+from backend.app.api.v1.endpoints import user_route
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, version="1.0.0")
-app.include_router(user.router, prefix="/api/v1")
+app.include_router(user_route.router, prefix="/api/v1")
 
 # app.dependency_overrides[get_session] = get_session
 
