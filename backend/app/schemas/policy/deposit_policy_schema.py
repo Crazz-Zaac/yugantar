eb7c6db9e67c372e42e4f63a62bdfe92e81fde60
crazz-zaac <r.alpha919@gmail.com>
2025-12-20 17:38:22 +0100

    Updated data type and respose schema changed #21

--- backend/app/schemas/policy/deposit_policy_schema.py
+++ backend/app/schemas/policy/deposit_policy_schema.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone
 from typing import Optional
 import uuid
 
+
 # -----------------------------
 # Create Schema
 # -----------------------------
@@ -10,12 +11,16 @@ class DepositPolicyCreate(SQLModel):
     deposit_amount_threshold: float
     late_deposit_fine: float  # in percentage
     deposit_frequency_days: int
+    
     effective_from: datetime = datetime.now(timezone.utc)
     effective_to: Optional[datetime] = None
+    
     is_active: bool = True
     is_occasional: bool = False
-    created_by: Optional[str] = None
     
+    created_by: Optional[str] = None
+
+
 # -----------------------------
 # Update Schema
 # -----------------------------
@@ -24,29 +29,38 @@ class DepositPolicyUpdate(SQLModel):
     late_deposit_fine: Optional[float] = None
     deposit_frequency_days: Optional[int] = None
     change_reason: str  # required for audit
+
     effective_from: Optional[datetime] = None
     effective_to: Optional[datetime] = None
+
     is_active: Optional[bool] = None
     is_occasional: Optional[bool] = None
-    
+
+    updated_at: datetime = datetime.now(timezone.utc)
+    updated_by: Optional[str] = None
+
+
 # -----------------------------
 # Read / Response Schema
 # -----------------------------
 class DepositPolicyResponse(SQLModel):
     policy_id: uuid.UUID
     deposit_amount_threshold: float
-    late_deposit_fine: float    # in percentage
+    late_deposit_fine: float  # in percentage
     deposit_frequency_days: int
-    change_reason: str
     version: int
+
     effective_from: datetime
     effective_to: Optional[datetime]
+
     is_active: bool
     is_occasional: bool
+
     created_by: Optional[str]
-    updated_by: Optional[str]
-    created_at: datetime
-    updated_at: datetime
+    created_at: Optional[datetime] = None
+
+    updated_by: Optional[str] = None
+    updated_at: Optional[datetime] = None
 
     class Config:
         from_attributes = True
