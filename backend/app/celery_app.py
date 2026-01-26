25c22ee9075d52ab016618841d54329a93a0ec93
crazz-zaac <r.alpha919@gmail.com>
2026-01-26 16:26:21 +0100

    reads all celery tasks

---
+++ backend/app/celery_app.py
@@ -0,0 +1,11 @@
+from celery import Celery
+import os
+
+
+celery_app = Celery(
+    "backend",
+    broker=os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0"),
+    backend=os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0"),
+)
+
+celery_app.autodiscover_tasks(["app.tasks"])
